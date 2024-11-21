7de9dd19b2f922856e82a5763aad61fcb9b2546c
Watson <599714@cjuhsd.net>
2024-11-21 09:43:26 -0800

    Fixed birthday files

--- 12. Happy Birthday Part 1.py
+++ 12. Happy Birthday Part 1.py
@@ -1,6 +1,6 @@
 z=input("What is your name? ")
 x=input("Is today your birthday? ")
-y=input("How old are you turning? ")
-y=int(y)
 if x=="Yes":
+    y=input("How old are you turning? ")
+    y=int(y)
     print(f"Happy Birthday {z} congrats on your {y}th birthday!!!")
