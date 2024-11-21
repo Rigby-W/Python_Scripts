30c05fcb6ef03613a1a89046f838a7d2c5337114
Watson <599714@cjuhsd.net>
2024-11-21 09:52:37 -0800

    Added some new files and edited certian files to account for uppercase

---
+++ 18. Hello Alice or Bob.py
@@ -0,0 +1,2 @@
+x = input("What is your name? ")
+if x.upper()=="alice".upper() or x.upper()=="bob".upper(): print("Hello, " + x)
---
+++ 19. 20 and 4.py
@@ -0,0 +1,5 @@
+x=input("Input number here: ")
+x=int(x)
+y=x%4
+if x>20 and y==0: 
+    print(f"{x} is greater than 20 and divisible by 4")
