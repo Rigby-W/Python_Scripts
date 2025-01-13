99c8e9fa000959ce7db481e716a84f959dce05bb
Rigby-W <furiousbuilder128@gmail.com>
2025-01-13 09:12:40 -0800

    Update 0. Test and Tips.py
    Added a tarot card generator complete with updating the hint file

--- 0. Test and Tips.py
+++ 0. Test and Tips.py
@@ -42,6 +42,7 @@ if password.upper()=="RigbyW".upper():
         print("dice")
         print("military")
         print("guess")
+        print("tarot")
         print("hint")
         print("-------------")        
         filetype=input("which testing file do you wish to access? ")
@@ -141,6 +142,26 @@ if password.upper()=="RigbyW".upper():
                     print(f"{response} is greater than the answer you have {10-x} guesses left")
                 elif response < value:
                     print(f"{response} is less than than the answer you have {10-x} guesses left")
+        elif filetype.upper()=="tarot".upper():
+            Minor_Arcana =('Ace of Swords','Two of Swords','Three of Swords','Four of Swords','Five of Swords','Six of Swords','Seven of Swords','Eight of Swords','Nine of Swords','Ten of Swords','Page of Swords','Knight of Swords','Queen of Swords','King of Swords','Ace of Wands','Two of Wands','Three of Wands','Four of Wands','Five of Wands','Six of Wands','Seven of Wands','Eight of Wands','Nine of Wands','Ten of Wands','Page of Wands','Knight of Wands','Queen of Wands','King of Wands','Ace of Pentacles','Two of Pentacles','Three of Pentacles','Four of Pentacles','Five of Pentacles','Six of Pentacles','Seven of Pentacles','Eight of Pentacles','Nine of Pentacles','Ten of Pentacles','Page of Pentacles','Knight of Pentacles','Queen of Pentacles','King of Pentacles','Ace of Cups','Two of Cups','Three of Cups','Four of Cups','Five of Cups','Six of Cups','Seven of Cups','Eight of Cups','Nine of Cups','Ten of Cups','Page of Cups','Knight of Cups','Queen of Cups','King of Cups')
+            Major_Arcana = ('The Magician','The High Priestess','The Empress','The Emperor','The Hierophant','The Lovers','The Chariot','Strength','The Hermit','Wheel of Fortune','Justice','The Hanged Man','Death','Temperance','The Devil','The Tower','The moon','The sun','Judgement','The World')
+            All_Arcana = ('Ace of Swords','Two of Swords','Three of Swords','Four of Swords','Five of Swords','Six of Swords','Seven of Swords','Eight of Swords','Nine of Swords','Ten of Swords','Page of Swords','Knight of Swords','Queen of Swords','King of Swords','Ace of Wands','Two of Wands','Three of Wands','Four of Wands','Five of Wands','Six of Wands','Seven of Wands','Eight of Wands','Nine of Wands','Ten of Wands','Page of Wands','Knight of Wands','Queen of Wands','King of Wands','Ace of Pentacles','Two of Pentacles','Three of Pentacles','Four of Pentacles','Five of Pentacles','Six of Pentacles','Seven of Pentacles','Eight of Pentacles','Nine of Pentacles','Ten of Pentacles','Page of Pentacles','Knight of Pentacles','Queen of Pentacles','King of Pentacles','Ace of Cups','Two of Cups','Three of Cups','Four of Cups','Five of Cups','Six of Cups','Seven of Cups','Eight of Cups','Nine of Cups','Ten of Cups','Page of Cups','Knight of Cups','Queen of Cups','King of Cups','The Magician','The High Priestess','The Empress','The Emperor','The Hierophant','The Lovers','The Chariot','Strength','The Hermit','Wheel of Fortune','Justice','The Hanged Man','Death','Temperance','The Devil','The Tower','The moon','The sun','Judgement','The World')
+            arcana=input("Would you like only Major, Minor, or All? ")
+            reverse_test=(random.randrange(1, 3, 1))
+            if reverse_test==1:
+                if arcana.upper()=="major".upper():
+                    print(f"Your card is {random.choice(Major_Arcana)} reversed")
+                elif arcana.upper()=="minor".upper():
+                    print(f"Your card is {random.choice(Minor_Arcana)} reversed")
+                elif arcana.upper()=="All".upper():
+                    print(f"Your card is {random.choice(All_Arcana)} reversed")
+            elif reverse_test!=1:
+                if arcana.upper()=="major".upper():
+                    print(f"Your card is {random.choice(Major_Arcana)} upright")
+                elif arcana.upper()=="minor".upper():
+                    print(f"Your card is {random.choice(Minor_Arcana)} upright")
+                elif arcana.upper()=="All".upper():
+                    print(f"Your card is {random.choice(All_Arcana)} upright")
         elif filetype.upper()=="hint".upper():
             filehint=input("what file do you wish to learn about? ")
             if filehint.upper()=="add".upper():
@@ -165,6 +186,8 @@ if password.upper()=="RigbyW".upper():
                 print("Changes the inputed time to military time")
             elif filehint.upper()=="guess".upper():
                 print("Opens a guessing game where you have to guess a number 1-100")
+            elif filehint.upper()=="tarot".upper():
+                print("Gives you a random tarot card")
             elif filehint.upper()=="hint".upper():
                 print("displays what a file does in cae you do not know what it does")
         retry=input("do you wish to access a new file? [y/n] ")
