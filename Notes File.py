d328877a0d626cda055976f272c2457cfffdd9a2
Watson <599714@cjuhsd.net>
2025-01-16 10:30:45 -0800

    added some new notes and explained a few more

--- Notes File.py
+++ Notes File.py
@@ -6,6 +6,8 @@
 # x>y is x is greater than y
 # x%y is to find the remainder
 # x**y is x to the power of y
+# x+=y is adding x to y permanently (used in loops usually)
+# x!=y is x doesn't equal y
 # useful terminal commands: 
 # "python": runs the python command with the same starting digit
 # "clear" : clears the termial to make it look cleaner
@@ -15,8 +17,9 @@
 # str=string (words and numbers allowed but cant be used for math) 
 # float= a floating integer (useful for precise calculations)
 # github commands:
-# git add (file name)
-# git commit -m "(share comment on changelog)"
-# git push -u origin main
-# git pull --all
-# git cd (name of repository)
+# git add "(file name)"   (adds a file to the repository)
+# git commit -m "(share comment on changelog)"  (tells you what you changed)
+# git push -u origin main   (pushes the new version to the repository)
+# git pull --all    (pulls any new versions from the repository to your computer)
+# git cd "(name of repository)"   (needed to run a command in the repsoitory file instead of the file it is located in)
+# git clone     (creates a copy of your repository that you can edit)
