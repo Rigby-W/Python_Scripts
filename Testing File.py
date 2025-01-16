936ff9c8b132f56d92c64b2b435772180aa97d69
Watson <599714@cjuhsd.net>
2025-01-16 10:17:28 -0800

    added a seperate notes file

--- Testing File.py
+++ Testing File.py
@@ -1,25 +1,3 @@
-# math commands:
-# x<=y is x is less than or equal to y
-# x>=y is x is greater than or equal to y
-# x==y is x is equal to y
-# x<y is x is less than y
-# x>y is x is greater than y
-# x%y is to find the remainder
-# x**y is x to the power of y
-# useful terminal commands: 
-# "python": runs the python command with the same starting digit
-# "clear" : clears the termial to make it look cleaner
-# "echo" : echoes text into the terminal
-# Variable types: 
-# int=integer (number only)
-# str=string (words and numbers allowed but cant be used for math) 
-# float= a floating integer (useful for precise calculations)
-# github commands:
-# git add (file name)
-# git commit -m "(share comment on changelog)"
-# git push -u origin main
-# git pull --all
-# git cd (name of repository)
 from datetime import datetime
 import time
 import random
