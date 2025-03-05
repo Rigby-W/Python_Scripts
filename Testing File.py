58d4275a3082afadc9179fb1fb6707c68cea5938
Rigby :3 <furiousbuilder128@gmail.com>
2025-03-05 13:32:01 -0800

    Removed the Rock Paper Scissors

--- Testing File.py
+++ Testing File.py
@@ -96,7 +96,6 @@ if password.upper()=="RigbyW".upper():
             print("Current games")
             print("-------------------------")
             print("Guessing Game (or GG)")
-            print("Rock Paper Scissors (or RPS)")
             print("-------------------------")
             gametype=input("What game would you like to play? ")
             if gametype.upper()=="guessing game".upper() or gametype.upper()=="GG".upper():
@@ -113,45 +112,6 @@ if password.upper()=="RigbyW".upper():
                         print(f"{response} is greater than the answer you have {10-x} guesses left")
                     elif response < value:
                         print(f"{response} is less than than the answer you have {10-x} guesses left")
-            if gametype.upper()=="Rock Paper Scissors".upper() or gametype.upper()=="RPS".upper():
-                opponent_choice=(random.randrange(1, 4, 1))
-                player_choice=input("Rock Paper or Scissors? (you can use R, P, or S respectively instead)")
-                if opponent_choice==1:
-                    if player_choice.upper()=="R".upper() or "Rock".upper():
-                        print("The opponent chose rock")
-                        print("You tie. Try again")
-                    if player_choice.upper()=="P".upper() or "Paper".upper():
-                        print("The opponent chose rock")
-                        print("You win. Good job")
-                    if player_choice.upper()=="S".upper() or "Scissors".upper():
-                        print("The opponent chose rock")
-                        print("You lose. Game over")
-                    else:
-                        print("error")
-                elif opponent_choice==2: 
-                    if player_choice.upper()=="R".upper() or "Rock".upper():
-                        print("The opponent chose paper")
-                        print("You lose. Game over")
-                    if player_choice.upper()=="P".upper() or "Paper".upper():
-                        print("The opponent chose paper")
-                        print("You tie. Try again")
-                    if player_choice.upper()=="S".upper() or "Scissors".upper():
-                        print("The opponent chose paper")
-                        print("You win. Good job")
-                    else:
-                        print("error")
-                elif opponent_choice==3:
-                    if player_choice=="R".upper() or "Rock".upper():
-                        print("The opponent chose scissors")
-                        print("You win. Good job")
-                    if player_choice=="P".upper() or "Paper".upper():
-                        print("The opponent chose scissors")
-                        print("You lose. Game over")
-                    if player_choice=="S".upper() or "Scissors".upper():
-                        print("The opponent chose scissors")
-                        print("You tie. Try again")
-                    else:
-                        print("error")
         elif filetype.upper()=="tarot".upper():
             Minor_Arcana =('Ace of Swords','Two of Swords','Three of Swords','Four of Swords','Five of Swords','Six of Swords','Seven of Swords','Eight of Swords','Nine of Swords','Ten of Swords','Page of Swords','Knight of Swords','Queen of Swords','King of Swords','Ace of Wands','Two of Wands','Three of Wands','Four of Wands','Five of Wands','Six of Wands','Seven of Wands','Eight of Wands','Nine of Wands','Ten of Wands','Page of Wands','Knight of Wands','Queen of Wands','King of Wands','Ace of Pentacles','Two of Pentacles','Three of Pentacles','Four of Pentacles','Five of Pentacles','Six of Pentacles','Seven of Pentacles','Eight of Pentacles','Nine of Pentacles','Ten of Pentacles','Page of Pentacles','Knight of Pentacles','Queen of Pentacles','King of Pentacles','Ace of Cups','Two of Cups','Three of Cups','Four of Cups','Five of Cups','Six of Cups','Seven of Cups','Eight of Cups','Nine of Cups','Ten of Cups','Page of Cups','Knight of Cups','Queen of Cups','King of Cups')
             Major_Arcana = ('The Magician','The High Priestess','The Empress','The Emperor','The Hierophant','The Lovers','The Chariot','Strength','The Hermit','Wheel of Fortune','Justice','The Hanged Man','Death','Temperance','The Devil','The Tower','The moon','The sun','Judgement','The World')
