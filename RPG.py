4ade70ef92a491d8cee441d2870f71a8948ec404
Watson <599714@cjuhsd.net>
2025-03-14 10:37:13 -0700

    working on a basic rpg game

--- RPG.py
+++ RPG.py
@@ -1,3 +1,4 @@
+import time
 name=input("What is your name? ")
 gender=input("What is your gender? Male Female Or Other? ")
 if gender.upper()=="Male".upper():
@@ -16,26 +17,58 @@ if gender.upper()=="Other".upper():
     pronoun1=("they")
     pronoun2=("them")
     pronoun3=("theirs")
-    pronoun4=("mix")
-    pronoun5=("lord")
+    pronoun4=("lord")
+    pronoun5=("mix")
 name=name.capitalize()
 gold=0
-print(f"Welcome {pronoun5} {name} to the wonderful world of Adventuria.")
+print(f"Welcome {pronoun4} {name} to the wonderful world of Adventuria.")
+time.sleep(1)
 print(f"You wake up in a field dazed with only the memory of your name, {name}.")
 print("-------------------------")
 print("a town")
 print("a forest")
 print("-------------------------")
-choice1=input("What do you see? ")
-if choice1.upper()=="a town".upper():
+start=input("What do you see? ")
+if start.upper()=="a town".upper():
     print(f"You see a town in the distance and you start walking towards the town and you find some money on the floor")
     loot=5
     gold+=loot
+    time.sleep(0.75)
     print(f"You collected {loot} gold, you have {gold} gold!")
-    print("-------------------------")
-    print("a store")
-    print("a house")
-    print("a pub")
-    print("-------------------------")
-if choice1.upper()=="a forest".upper():
+    in_town=True
+    house=0
+    while in_town==True:    
+        print("-------------------------")
+        print("a store")
+        print("a house")
+        print("a pub")
+        print("-------------------------")
+        town_location=input("Where do you wish to go? ")
+        time.sleep(0.75)
+        if town_location.upper()=="a store".upper() and gold>=4:
+            print(f"You walk into the store and pick up some groceries for 4 gold, you have {gold-4} gold!")
+            gold-=4
+            time.sleep(0.5)
+        if town_location.upper()=="a store".upper() and gold<4:
+            print("You walk into the store but sadly you do not have enough money so you leave ")
+            time.sleep(0.5)
+        if town_location.upper()=="a house".upper() and house!=1:
+            print("You walk into an old abandoned house and decide to stay there for the night and find a few pieces of gold")
+            loot=7
+            gold+=loot
+            print(f"You collected {loot} gold, you have {gold} gold!")
+            house=1
+            time.sleep(0.75)
+        if town_location.upper()=="a house".upper() and house==1:
+            print("You walk into your house and rest for a bit")
+            time.sleep(1.5)
+        if town_location.upper()=="a pub".upper():
+            print("You walk into a pub and meet the bartender, Henry")
+            print(f"Henry- 'Oh hello there {pronoun5} {name}'")
+            Drink_choice=input("Would you like something to drink its on the house? ")
+            if Drink_choice.upper=="Y".upper() or "Yes".upper():
+                print("You take a drink")
+            if Drink_choice.upper=="n".upper() or "no".upper():
+                print("You decline his offer to take a drink and leave")
+if start.upper()=="a forest".upper():
     print("You walk into the forest")
